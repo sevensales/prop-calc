613183addd76a26be922c2eea6d85b64bd84ba79
José Carlos Marquetti Junior <iwasthesword@users.noreply.github.com>
2023-03-29 09:49:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Container,
+  NumberInput,
+  NumberInputField,
+  FormControl,
+  Flex,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
+  let params = useParams();
+  let data = params.str.split("|");
+  let ingredients = data[0].split(";");
+  let recipe = data[1];
+  ingredients.forEach(function (element, index) {
+    ingredients[index] = element.split(",");
+  });
+  const [inputValue, setInputValue] = useState(10.5);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container maxW="container.lg">
+        <Heading mb={5}>{recipe}</Heading>
+        <FormControl mb={5}>
+          <Flex alignItems="center" gap={3}>
+            <Text as='b'>Peso em Kilogramas:</Text>
+            <NumberInput
+              precision={2}
+              step={0.2}
+              type="text"
+              id="inputField"
+              onChange={(valueString) => setInputValue(valueString)}
+              flex={1}
+            >
+              <NumberInputField />
+            </NumberInput>
+          </Flex>
+        </FormControl>
+        <TableContainer>
+          <Table variant="striped" size="sm">
+            <Thead>
+              <Tr>
+                <Th>Ingredientes</Th>
+                <Th isNumeric>Gramas</Th>
+                <Th isNumeric>Kilos</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {ingredients.map(function (ingredient, i) {
+                return (
+                  <Tr>
+                    <Td>{ingredient[0]}</Td>
+                    <Td isNumeric>
+                      {inputValue
+                        ? (ingredient[1] * (inputValue * 1000)).toFixed(2)
+                        : 0}
+                    </Td>
+                    <Td isNumeric>
+                      {inputValue ? (ingredient[1] * inputValue).toFixed(2) : 0}
+                    </Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path=":str" element={<App />} />
+        </Routes>
+      </HashRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
